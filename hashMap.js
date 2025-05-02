f40b1591b99f5900e46cb28486ddca6aad899e01
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-05-02 08:49:38 -0600

    Add hash function, initialize buckets

--- hashMap.js
+++ hashMap.js
@@ -1,8 +1,22 @@
 export default class HashMap {
-    constructor(loadFactor, capacity) {
+    constructor(loadFactor = 0.75, capacity = 16) {
         this.loadFactor = loadFactor;
         this.capacity = capacity;
+        this.buckets = new Array(this.capacity).fill(null);
+        this.size = 0;
     }
+
+    // takes a key and produces a hash code
+    hash(key) {
+        let hashCode = 0;
+           
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+     
+        return hashCode;
+    } 
 
     static mapName = "The name's map. Hash map.";
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import HashMap from "./hashMap.js";
 const map = new HashMap;
 console.log(HashMap.mapName);
-console.log(map);
+console.log(map.hash("Peter"));
