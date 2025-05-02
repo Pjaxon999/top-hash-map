7c07665627f99420279a2703b5acc25c3ed058bb
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-05-02 08:26:58 -0600

    Initialize project, create base files and link

---
+++ hashMap.js
@@ -0,0 +1,8 @@
+export default class HashMap {
+    constructor(loadFactor, capacity) {
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+    }
+
+    static mapName = "The name's map. Hash map.";
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import HashMap from "./hashMap.js";
+const map = new HashMap;
+console.log(HashMap.mapName);
+console.log(map);
